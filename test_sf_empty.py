7b8c4d051050966c62d886eb4a8980cf4b7db7a1
Shajid <shajidmosharraf@gmail.com>
2026-09-25 15:47:54 +0600

    frontend upgrade

--- test_sf_empty.py
+++
@@ -1,9 +0,0 @@
-import soundfile as sf
-import io
-import numpy as np
-buf = io.BytesIO()
-try:
-    sf.write(buf, np.zeros(0), 16000, format='WAV')
-    print('Success', len(buf.getvalue()))
-except Exception as e:
-    print('Error:', e)
